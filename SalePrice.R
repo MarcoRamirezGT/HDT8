037f4768cb60510969e581df5fb85ed08f73b8b0
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2022-04-29 20:15:10 -0600

    Update SalePrice.R

--- SalePrice.R
+++ SalePrice.R
@@ -12,7 +12,7 @@ data$GarageCars<-as.numeric(data$GarageCars)
 data$YearBuilt<-as.numeric(data$YearBuilt)
 data$GarageArea<-as.numeric(data$GarageArea)
 data$X1stFlrSF<-as.numeric(data$X1stFlrSF)
-
+#Funcion para normalizar los datos
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
@@ -21,7 +21,9 @@ normalize <- function(x) {
 
 porcentaje<-0.7
 set.seed(123)
+#Creamos el dataframe para predecir
 datos2<-data.frame(data$GrLivArea,data$GarageCars,data$YearBuilt,data$GarageArea,data$X1stFlrSF,data$SalePrice)
+#Creamos el dataframe con los datos normalizados
 datos <- as.data.frame(lapply(datos2, normalize))
 
 
@@ -32,18 +34,19 @@ test<-datos[-corte,]
 #-------------------------------------------------
 # Red Neuronal con nnet
 #-------------------------------------------------
-
+#Prediccion
 modelo.nn2 <- nnet(as.numeric(data.SalePrice)~.,data = datos,subset = corte, size=2, rang=0.1,decay=5e-4, maxit=200) 
 prediccion1 <- predict(modelo.nn2, newdata = test,na.rm=TRUE)
 prediccion1
-
+#Desnormalizar datos
 minvec <- sapply(datos2,min)
 maxvec <- sapply(datos2,max)
 denormalize <- function(x,minval,maxval) {
   x*(maxval-minval) + minval
 }
-
+#Desnormalizamos el dataframe
 datos<-as.data.frame(Map(denormalize,datos,minvec,maxvec))
+#Desnormalizamos la prediccion
 prediccion1_r<-(prediccion1)*(max(datos2)-min(datos2))+min(datos2)
 
 plot(datos$data.SalePrice)
